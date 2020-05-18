48a8e396110d4b0c97159659b85febcc9c867a45
o.karpik <o.karpik@bars-open.ru>
2020-05-18 07:51:46 +0700

    module4-task1

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports.DEFAULT_COMMAND = `--help`;
+
+module.exports.USER_ARGV_INDEX = 2;
+
+module.exports.ExitCode = {
+  error: 1,
+  success: 0,
+};
+
+module.exports.HttpCode = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
+module.exports.API_PREFIX = `/api`;
+
+module.exports.MAX_ID_LENGTH = 6;
+
+module.exports.MAX_COMMENTS = 4;
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,34 +2,38 @@
 
 const chalk = require(`chalk`);
 const express = require(`express`);
-const app = express();
-const fs = require(`fs`).promises;
+const {HttpCode, API_PREFIX} = require(`../../constants`);
+const routes = require(`../api`);
 
 const DEFAULT_PORT = 3000;
-const FILENAME = `mocks.json`;
 
-const onClientConnect = async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    res.send(JSON.parse(fileContent));
-  } catch (err) {
-    res.send([]);
-  }
-};
+const app = express();
+
+app.use(express.json());
+app.use(API_PREFIX, routes);
+
+app.use((req, res) => res
+.status(HttpCode.NOT_FOUND)
+.send(`Not found`));
 
 module.exports = {
   name: `--server`,
-  run(args) {
+  async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    app.listen(port).on(`listening`, (err) => {
-      if (err) {
-        return console.error(`Ошибка при создании сервера`, err);
-      }
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return console.error(`Ошибка при создании сервера`, err);
+        }
 
-      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-    });
-    app.get(`/offers`, onClientConnect);
+        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+      });
+
+    } catch (err) {
+      console.error(`Произошла ошибка: ${err.message}`);
+      process.exit(1);
+    }
   }
 };
